155b5b59fd80f504382152f9f163c3932f007f71
tonyyystep <tonyyystep@gmail.com>
2021-07-25 19:00:49 +0300

    mypolygon.py

--- mypolygon.py
+++ mypolygon.py
@@ -1,19 +1,38 @@
+
 import turtle
+import math
 bob = turtle.Turtle()
-print(bob)
-def square(t, lenght):
+alice = turtle.Turtle()
   
+
+def square(t, length):
     for i in range(4):
-        print('Hello')
-        bob.fd(lenght)
-        bob.lt(90)
-        
+        t.fd(length)
+        t.lt(90)
 
-def polygon(t, n, z):
-  
+
+
+def polyline(t, n, length, angle):
+    """Рисует n отрезков с заданной длинной lenght и углами angle (в градусах) между ними.
+         t - это черепашка """
     for i in range(n):
-        bob.fd(z)
-        bob.lt(360/n)
+        t.fd(length)
+        t.lt(angle)
 
-polygon(bob, 10, 100)
+def polygon(t, n, length):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)   
+
+def circle(t, r):
+    arc(t, r, 360)
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n 
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+circle(bob, 30)
+circle(alice, 100)
+
 turtle.mainloop()
