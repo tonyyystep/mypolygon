d5cfcf3a81bd55d8ad6cab350bf53939d9b3e18f
tonyyystep <tonyyystep@gmail.com>
2021-07-25 19:11:12 +0300

    mypolygon.py

--- mypolygon.py
+++ mypolygon.py
@@ -6,6 +6,7 @@ alice = turtle.Turtle()
   
 
 def square(t, length):
+    # Рисует квадрат с length(длиной сторон)
     for i in range(4):
         t.fd(length)
         t.lt(90)
@@ -20,19 +21,26 @@ def polyline(t, n, length, angle):
         t.lt(angle)
 
 def polygon(t, n, length):
+    """Рисует многоугольник с n количеством сторон и lenght длиной сторон"""
     angle = 360.0 / n
     polyline(t, n, length, angle)   
 
 def circle(t, r):
+    """Рисует круг r радиуса"""
     arc(t, r, 360)
 
 def arc(t, r, angle):
+    """Рисует дугу с заданным радиусом и углом.
+    t: Черепаха
+    r: радиус
+    angle: угол, образованный дугой, в градусах."""
     arc_length = 2 * math.pi * r * angle / 360
-    n = int(arc_length / 3) + 1
+    n = int(arc_length / 3) + 1s
     step_length = arc_length / n 
     step_angle = float(angle) / n
     polyline(t, n, step_length, step_angle)
+
 circle(bob, 30)
 circle(alice, 100)
-
+)
 turtle.mainloop()
